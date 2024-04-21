09c73e2a9d45bdbb64d6d394d9c238cc3e0adaa3
mjm95 <mjm121273@gmail.com>
2024-04-21 23:04:19 +0330

    Add files via upload
    initial commit

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,43 @@
+
+
+
+const scriptsInEvents = {
+
+	async Gbokseventsheet_Event6_Act1(runtime, localVars)
+	{
+		runtime.globalVars.ajaxHeader=window.parent.csrfToken;
+		window.parent.gboks.emit('ready');
+		
+		window.parent.gboks.emitter.addListener('fake-start', (e) => {
+			runtime.globalVars.fakesStart=true;
+		});
+		window.parent.gboks.emitter.addListener('real-start', (e) => {
+			runtime.globalVars.realStart=true;
+		});
+	},
+
+	async Gbokseventsheet_Event9_Act1(runtime, localVars)
+	{
+		// fake rotate done event bullshit
+		
+		window.parent.gboks.emit('fake-spin-done');
+		
+	},
+
+	async Gbokseventsheet_Event10_Act1(runtime, localVars)
+	{
+		window.parent.gboks.emit('real-spin-done',runtime.globalVars.giftIndex);
+		
+		
+		
+	},
+
+	async Gbokseventsheet_Event11_Act2(runtime, localVars)
+	{
+		runtime.globalVars.giftIndex=JSON.parse(runtime.globalVars.ajaxRes).gift;
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
